d0cac207631801fd1c2d7c3d1a9663efc9e58b3a
Alcy Torres <mxalcytorres@gmail.com>
2024-06-24 21:56:42 -0400

    show action

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -3,4 +3,10 @@ class PlayersController < ApplicationController
     @players = Player.all
     render :index
   end
+
+  def show
+    @player = Player.find_by(id: params[:id])
+    render :show
+  end
+
 end
